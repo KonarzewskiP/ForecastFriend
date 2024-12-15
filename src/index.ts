37d8b45d2651e13b6771f8daeb426e462cd06f7e
Pawel Konarzewski <pawelkonarzewski13@gmail.com>
2024-12-15 16:52:42 +0100

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 80;
+
+app.get('/', (req, res) => {
+  res.send('Hello, TypeScript!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
